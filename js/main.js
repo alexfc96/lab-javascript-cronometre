725a3538e1c46eedf60f13d6346d5f6d2bbb43ec
alex <alex@PC-AZUL.localdomain>
2020-01-17 19:17:32 +0100

    working the counter seconds to minutes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
+console.clear()
 
 class Timer {
-    constructor(myTimer, contTimer){
-        this.myTimer = 0
+    constructor(myTimer, contTimer, seconds, minutes, hours){
+        this.myTimer = 0;  
         this.contTimer = contTimer;
+        this.seconds = 0;
+        this.minutes = 0;
+        this.hours = 0;
     }
-
-
     start(){
-        this.contTimer = setInterval(function(){
+            this.contTimer = setInterval(function(){
             this.myTimer++;
-            console.log(this.myTimer)
+            this.seconds++;
+            this.showTime()
+            //console.log(this.myTimer)
             //return this.myTimer
         }.bind(this), 1000);
         //return contTimer
         }
 
     stop(){
-        clearInterval(this.contTimer);
-
+        clearInterval(this.contTimer); 
+        //clearInterval(this.start.contTimer); 
     }
+  
     reset(){
         this.myTimer = 0
-    }
-    /*
+    }   
     showTime(){
-        //const hms = [[][][]]
-        //this.constTimer;
-        const minutes = 0
-        if(this.myTimer === 60){
-            minutes = minutes++
-            console.log("Mins:{$minutes} : Seconds{$myTimer}")
+        console.log(this.hours + ":" + this.minutes + ":" + this.seconds);
+        if(this.seconds % 59 === 0){
+            this.seconds = 0
+            this.minutes++;
+            console.log(this.hours + ":" + this.minutes + ":" + this.seconds);
+            if(this.minutes % 59 === 0){
+                this.minutes = 0
+                this.hours++;
+            }
+            
         }
 
     }
-    */
 }
 
     
